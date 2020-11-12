6d333a6d9911320b9ea8114bc0001ab86783d53c
Husni Kamal <husnikamal3@gmail.com>
2020-11-12 15:48:12 +0700

    push constants for test purpose

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,2 +1,2 @@
-const String API_KEY = 'YOUR_API_KEY';
-const String NEWS_API_KEY = 'YOUR_API_KEY';
+const String API_KEY = '42ced69cd25eefef6baef5a672e1abb9';
+const String NEWS_API_KEY = 'a7e670365d50454a9d746ae81fbbae29';
